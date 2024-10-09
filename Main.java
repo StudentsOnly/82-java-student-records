5f06d5ce41bcae5aa7c10a889d3687887a86309f
MichaelJGKopp <michaeljg.kopp@gmail.com>
2024-10-09 11:04:44 +0200

    set up list of students

--- Main.java
+++ Main.java
@@ -1,2 +1,23 @@
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Main {
+
+  public static void main(String[] args) {
+
+    List<Student> students = IntStream.rangeClosed(0, 100)
+      .mapToObj(i -> Student.getRandom())
+      .toList();
+
+    System.out.printf("STUDENTS[%d]", students.size());
+    System.out.println("--------------------------------------");
+    students.stream()
+      .limit(10)
+      .forEach(System.out::println);
+    System.out.println();
+
+
+    System.out.println("--------------------------------------");
+  }
 }
